a51e860d89ab6dcca4ff929911781dbcb4f4495e
Alexey <ax.chinaev@yandex.ru>
2024-03-21 02:10:34 +0300

    fix

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -6,16 +6,13 @@ import (
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// frontAddr := os.Getenv("FRONTEND_SERVER_ADDR")
-
-		// w.Header().Set("Access-Control-Allow-Origin", frontAddr)
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Max-Age", "86400")
 
 		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, User-Agent, X-Requested-With")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS")
+			w.Header().Set("Access-Control-Max-Age", "86400")
 			w.WriteHeader(http.StatusOK)
 		} else {
 			next.ServeHTTP(w, r)
